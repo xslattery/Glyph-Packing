05b6ae6d060cf42cd8103dfed1bdd34065e35bed
Xavier S <xavier.slattery@gmail.com>
2018-01-13 16:56:47 +1100

    build fix

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ echo "Compiling Program on: "
 date
 
 DIR_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
-cd $DIR_PATH
+cd "$DIR_PATH"
 
 clang++ -arch x86_64 -std=c++14 -rpath @loader_path/../libs -I libs/include -L libs -framework OpenGL -l glfw33 -l freetyped2_7 src/main.cpp src/shader.cpp -o build/main
 
